cd7d76e9998d22a16f3573478000ad145d5efd75
SniperBlaze <72265661+shoddyDEV@users.noreply.github.com>
2021-10-17 10:39:24 +0530

    Create windows_installer.bat
    Use this windows batch file to prepare the script to run. Requires a Windows NT system.

---
+++ windows_installer.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Installing Pylance Libraries...
+timeout /t 1 /nobreak > nul
+pip install praw
+pip install discord
+pip install discord-py-slash-commmand
+pip install pickle
+pip install requests
+cls
+echo Library Installation Complete! Press any key to exit.
+pause > nul
+exit /b
