d90c7c3ba1079f62d54eb45840dcb017f60b3e59
SniperBlaze <72265661+shoddyDEV@users.noreply.github.com>
2021-10-17 10:47:00 +0530

    Create linux_installer.sh
    Linux script used to install python libraries to prepare script for running. Requires a machine running Linux and pip library installer.

---
+++ linux_installer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function try()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function throw()
+{
+    exit $1
+}
+
+function catch()
+{
+    export exception_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $exception_code
+}
+echo "- Installing modules..."
+try
+(
+    pip install discord > /dev/null
+    echo "- Installed discord!"
+    pip install praw > /dev/null
+    echo "- Installed praw!"
+    pip install discord-py-slash-command > /dev/null
+    echo "- Complete!"
+)
+catch || {
+   case $exc_code in
+        *)
+            echo "! Error installing modules"
+            throw $exc_code
+        ;;
+    esac
+}
