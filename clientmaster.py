099bbd2c8b840019e970ecc92e95afb2aecc3478
notsniped <72265661+notsniped@users.noreply.github.com>
2021-12-11 16:23:36 +0530

    Added clientmaster library (for bot configuration)
    Add things like your bot name, token, discriminator, app id, etc. here.

---
+++ clientmaster.py
@@ -0,0 +1,9 @@
+## Configure Startup Variables ##
+botname = ''
+botdiscrim = ''
+appid = ''
+publickey = ''
+clientid = ''
+clientsecret = ''
+bottoken = ''
+### INFO : ADD YOUR OWN INFORMATION IN THE FILE ###
