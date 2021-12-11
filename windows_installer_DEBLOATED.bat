0694f8024b3e6dd49c949096ba10a36cf62ca226
notsniped <72265661+notsniped@users.noreply.github.com>
2021-12-11 16:28:15 +0530

    Create windows_installer_DEBLOATED.bat
    Lightweight installer for python libraries

---
+++ windows_installer_DEBLOATED.bat
@@ -0,0 +1,8 @@
+@echo off
+pip install praw
+pip instll nekos.py
+pip install -U discord_py_slash_command
+pip install pickle
+pip install discord
+pip install pynacl
+echo Install complete. Exiting process...
